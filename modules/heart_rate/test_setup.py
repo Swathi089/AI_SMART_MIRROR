aeed327ec486f4b50dcb18cbe0b4f177b4f5fba9
Swathi <swatheswari2005@gmail.com>
2026-09-20 19:37:11 +0530

    Add heart rate environment and webcam tests

---
+++ modules/heart_rate/test_setup.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import scipy
+import mediapipe as mp
+
+print("OpenCV:", cv2.__version__)
+print("NumPy:", np.__version__)
+print("SciPy:", scipy.__version__)
+print("MediaPipe:", mp.__version__)
+
+print("Heart-rate environment is ready!")
